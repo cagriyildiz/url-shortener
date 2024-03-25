22b3a4a666f6e2a768ac73e76ac0a9cb03d87d5d
Cagri Yildiz <cagriyild@gmail.com>
2024-03-25 11:29:14 +0100

    Create health check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+mod routes;
+
+use std::error::Error;
+use axum::Router;
+use axum::routing::get;
+use tokio::net::TcpListener;
+use crate::routes::health;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let app = Router::new()
+        .route("/health", get(health));
+
+    let listener = TcpListener::bind("0.0.0.0:3000")
+        .await
+        .expect("Could not initialize TcpListener");
+
+    axum::serve(listener, app)
+        .await
+        .expect("Could not start the server");
+
+    Ok(())
 }
