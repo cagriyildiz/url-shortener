989ea20e9965a030304a6dd9d4d2d51ab864c084
cagriyildiz <cagriyild@gmail.com>
2024-03-25 15:56:19 +0100

    Create connection to the database

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,27 @@ mod routes;
 use std::error::Error;
 use axum::Router;
 use axum::routing::get;
+use dotenvy::dotenv;
+use sqlx::postgres::PgPoolOptions;
 use tokio::net::TcpListener;
 use crate::routes::health;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    dotenv().ok();
+
+    let db_url = std::env::var("DATABASE_URL")
+        .expect("Environment variable 'DATABASE_URL' is required");
+
+    let db_pool = PgPoolOptions::new()
+        .max_connections(20)
+        .connect(&db_url)
+        .await
+        .expect("Could not connect to the database");
+
     let app = Router::new()
-        .route("/health", get(health));
+        .route("/health", get(health))
+        .with_state(db_pool);
 
     let listener = TcpListener::bind("0.0.0.0:3000")
         .await
