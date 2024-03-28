e4f199fa07fbac3ab244c2657799f5e16dd6169b
Cagri Yildiz <cagriyild@gmail.com>
2024-03-28 10:31:00 +0100

    Collect link engagement statistics on redirect

--- src/routes.rs
+++ src/routes.rs
@@ -1,12 +1,14 @@
 use axum::body::Body;
 use axum::extract::{Path, State};
-use axum::http::StatusCode;
+use axum::http::{HeaderMap, StatusCode};
 use axum::Json;
 use axum::response::{IntoResponse, Response};
 use base64::Engine;
 use base64::engine::general_purpose;
 use rand::Rng;
-use sqlx::PgPool;
+use sqlx::{Error, PgPool};
+use sqlx::postgres::PgQueryResult;
+use tokio::time::error::Elapsed;
 use url::Url;
 use crate::utils::internal_error;
 use crate::model::{Link, LinkTarget};
@@ -61,6 +63,7 @@ pub async fn create_link(
 pub async fn redirect(
     State(pool): State<PgPool>,
     Path(requested_link): Path<String>,
+    headers: HeaderMap,
 ) -> Result<Response, (StatusCode, String)> {
     let timeout = tokio::time::Duration::from_millis(300);
 
@@ -80,6 +83,37 @@ pub async fn redirect(
     .map_err(|err| (StatusCode::NOT_FOUND, err))?;
 
     tracing::debug!("Redirecting link id {} to {}", requested_link, link.target_url);
+
+    let referer_header = headers
+        .get("referer")
+        .map(|val| val.to_str().unwrap_or_default().to_string());
+
+    let user_agent_header = headers
+        .get("user-agent")
+        .map(|val| val.to_str().unwrap_or_default().to_string());
+
+    let statistics = tokio::time::timeout(
+        timeout,
+        sqlx::query(
+            "insert into link_statistics(link_id, referer, user_agent) values($1, $2, $3)"
+        )
+        .bind(&link.id)
+        .bind(&referer_header)
+        .bind(&user_agent_header)
+        .execute(&pool)
+    )
+    .await;
+
+    match statistics {
+        Err(elapsed) => tracing::error!("Could not save new link click statistics due to a time out: {}", elapsed),
+        Ok(Err(err)) => tracing::error!("Could not save new link click statistics due to an error: {}", err),
+        _ => tracing::debug!(
+            "Persisted new link click statistics for the link with id {}, referer {}, user_agent {}",
+            requested_link,
+            referer_header.unwrap_or_default(),
+            user_agent_header.unwrap_or_default(),
+        )
+    }
 
     Ok(
         Response::builder()
